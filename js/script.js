d5e99ccc3a279d34ffec5048dbab2eeefdd88f5e
bio8oid <8bollod8@mail.co.uk>
2019-11-13 01:10:50 +0100

    Add link-modal

--- js/script.js
+++ js/script.js
@@ -4,92 +4,99 @@
   // Reset General Statistic Calendar \\
 
   if (window.location.href.indexOf('index') > -1 || window.location.href.indexOf('details') > -1 || window.location.href.indexOf('payout') > -1) {
-    document.getElementById("general-calendar-reset").addEventListener('click', function() {
+    document.getElementById("general-calendar-reset").addEventListener('click', ()  => {
       document.getElementById("general-calendar-form").reset();
     });
-  }
+  };
   
   // Hamburger menu function \\
 
-  function toggleMenu(visible) {
-    document.getElementById('sidebar-section').classList.toggle('show', visible)
-  }
+  const toggleMenu = visible => {
+    document.getElementById('sidebar-section').classList.toggle('show', visible);
+  };
 
-  document.getElementById('menu-burger').addEventListener('click', function (e) {
+  document.getElementById('menu-burger').addEventListener('click', e  => {
     e.preventDefault();
-    toggleMenu()
+    toggleMenu();
   });
 
   // Sidebar-burger menu function \\
 
-  function toggleSmallMenu(visible) {
-    document.getElementById('sidebar-section').classList.toggle('sidebar-small', visible)
+  const toggleSmallMenu = visible => {
+    document.getElementById('sidebar-section').classList.toggle('sidebar-small', visible);
 
-    document.body.classList.toggle('widen')
-  }
+    document.body.classList.toggle('widen');
+  };
 
-  document.getElementById('sidebar-burger').addEventListener('click', function (e) {
+  document.getElementById('sidebar-burger').addEventListener('click', e  => {
     e.preventDefault();
-    toggleSmallMenu()
+    toggleSmallMenu();
   });
 
 
   // Modals \\
 
 
-  function closeModal() {
-    document.getElementById('overlay').classList.remove('show')
+  const closeModal = () => {
+    document.getElementById('overlay').classList.remove('show');
   }
 
-  document.querySelectorAll('#overlay .js--close-modal').forEach(function (btn) {
-    btn.addEventListener('click', function (e) {
-      e.preventDefault()
-      closeModal()
-    })
-  })
+  document.querySelectorAll('#overlay .js--close-modal').forEach(btn => {
+    btn.addEventListener('click', e  => {
+      e.preventDefault();
+      closeModal();
+    });
+  });
 
-  document.querySelector('#overlay').addEventListener('click', function (e) {
+  document.querySelector('#overlay').addEventListener('click', e  => {
     if (e.target === this) {
-      closeModal()
-    }
-  })
+      closeModal();
+    };
+  });
 
-  document.addEventListener('keyup', function (e) {
+  document.addEventListener('keyup', e  => {
     if (e.keyCode === 27) {
-      closeModal()
-    }
-  })
+      closeModal();
+    };
+  });
 
-  function openModal(modal) {
-    document.querySelectorAll('#overlay > *').forEach(function (modal) {
-      modal.classList.remove('show')
-    })
-    document.querySelector('#overlay').classList.add('show')
-    document.querySelector(modal).classList.add('show')
-  }
-
-    document.getElementById('exit').addEventListener('click', function (e) {
-      e.preventDefault()
-      openModal('#myModal')
+  const openModal = modal => {
+    document.querySelectorAll('#overlay > *').forEach(modal => {
+      modal.classList.remove('show');
     });
-  
-    document.getElementById('login').addEventListener('click', function (e) {
-      e.preventDefault()
-      openModal('#loginModal')
-    });
-
-    document.getElementById('exit-sidebar').addEventListener('click', function (e) {
-      e.preventDefault()
-      openModal('#myModal')
-    });
-  
-    document.getElementById('login-sidebar').addEventListener('click', function (e) {
-      e.preventDefault()
-      openModal('#loginModal')
-    });
-
-    document.getElementById('contact-manager').addEventListener('click', function (e) {
-      e.preventDefault()
-      openModal('#manager')
-    });
+    document.querySelector('#overlay').classList.add('show');
+    document.querySelector(modal).classList.add('show');
   };
+
+    document.getElementById('exit').addEventListener('click', e  => {
+      e.preventDefault();
+      openModal('#myModal');
+    });
+  
+    document.getElementById('login').addEventListener('click', e  => {
+      e.preventDefault();
+      openModal('#loginModal');
+    });
+
+    document.getElementById('exit-sidebar').addEventListener('click', e  => {
+      e.preventDefault();
+      openModal('#myModal');
+    });
+  
+    document.getElementById('login-sidebar').addEventListener('click', e  => {
+      e.preventDefault();
+      openModal('#loginModal');
+    });
+
+    document.getElementById('contact-manager').addEventListener('click', e  => {
+      e.preventDefault();
+      openModal('#manager');
+    });
+
+    document.querySelectorAll('.add-link-button').forEach(item => {
+    item.addEventListener('click', e  => {
+      e.preventDefault();
+      openModal('#add-link');
+    });
+  });
+};
