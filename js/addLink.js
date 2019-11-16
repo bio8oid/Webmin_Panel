f418055f749241e66b3898abe4f195537f7110bd
bio8oid <8bollod8@mail.co.uk>
2019-11-16 21:35:05 +0100

    Add working add link to links table feature and form validation

--- js/addLink.js
+++ js/addLink.js
@@ -1,44 +1,73 @@
-const newElement = () => {
-    const linkTable = document.getElementById('link-table'); 
-    const newLine = document.createElement('div');
-    const newName = document.createElement('div');
-    const newLink = document.createElement('div');
+const createElement = () => {
+
+    // Links Table Element
+    const linkTable = document.getElementById('link-table');
+
+    // New Table Row Element
+    const newTableRow = document.createElement('div');
+
+    // New Name Element
     const newNameParaghraph = document.createElement('p');
     const nameInputValue = document.getElementById('link-name-input').value;
     const nameValue = document.createTextNode(nameInputValue);
-    
+    newNameParaghraph.appendChild(nameValue);
+    const newName = document.createElement('div');
+    newName.appendChild(newNameParaghraph);
+    newName.classList.add('name-column');
+    newTableRow.appendChild(newName);
+    newTableRow.classList.add('table-row');
+
+    // New Link Element
     const link = document.createElement('a');
     const linkInputValue = document.getElementById('link-value-input').value;
     const LinkText = document.createTextNode(linkInputValue);
-
-    newNameParaghraph.appendChild(nameValue);
-    newName.appendChild(newNameParaghraph);
-    newName.classList.add('name-column');
-    newLine.appendChild(newName);
-    newLine.classList.add('table-row');
-    
     link.setAttribute('href', linkInputValue);
     link.setAttribute('target', "_blank");
     link.appendChild(LinkText);
+    const newLink = document.createElement('div');
     newLink.appendChild(link);
     newLink.classList.add('link-column');
-    newLine.appendChild(newLink);
-    
-    linkTable.appendChild(newLine);
+    newTableRow.appendChild(newLink);
 
+    // New Icons Element
+    const newIcons = document.createElement('div');
 
-    // link.appendChild(LinkValue);
-    // link.setAttribute('href', LinkValue);
-    // link.setAttribute('target', "_blank");
-    // newLink.appendChild(link);
-    // newLink.classList.add('link-column');
-    // linkTable.appendChild(newLink);
-    
-    // newLine.appendChild(newName);
-    // newLine.classList.add('table-row');
+    // Link Icon
+    const linkIcon = document.createElement('i');
+    linkIcon.classList.add('icon-links');
+    const linkElement = document.createElement('a');
+    linkElement.appendChild(linkIcon);
+    newIcons.appendChild(linkElement);
 
-    // div.setAttribute('class', 'note');
+    // Trash Icon
+    const trashIcon = document.createElement('i');
+    trashIcon.classList.add('icon-trash');
+    const trashElement = document.createElement('a');
+    trashElement.appendChild(trashIcon);
+    trashElement.classList.add('trash-button');
+    trashElement.addEventListener('click', removeElement);
+    newIcons.appendChild(trashElement);
+    newIcons.classList.add('links-and-bin-column');
+    newTableRow.appendChild(newIcons);
+
+    if (linkInputValue.length && nameInputValue.length > 0) {
+        linkTable.appendChild(newTableRow);
+    } else {
+        alert("Enter Name and URL")
+    }
 }
 
+const removeElement = (e) => {
+    const toRemove = e.target;
+    toRemove.parentNode.parentNode.parentNode.parentNode.removeChild(toRemove.parentNode.parentNode.parentNode);
+}
+
+// Add New Link Button
 const addLinkButton = document.getElementById('add-link-button');
-addLinkButton.addEventListener('click', newElement);
+addLinkButton.addEventListener('click', createElement);
+
+// Remove Link Button
+const removeLinkButtons = document.querySelectorAll('.trash-button');
+for (const button of removeLinkButtons) {
+    button.addEventListener('click', removeElement);
+}
